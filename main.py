9d8604a49f62831073672d360326ecf6c6b9e14c
bip-zip <bipindhakal44@gmail.com>
2023-04-29 09:28:29 +0545

    register model

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-
+from models.users import *
+from tortoise.contrib.fastapi import register_tortoise
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
+async def index():
     return {"Good": "Feelings"}
 
 
-
+register_tortoise(
+    app,
+    db_url='sqlite://database.sqlite3',
+    modules= {'models':["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
 
--- models/users.py
+++ models/users.py
@@ -1,6 +1,6 @@
 from tortoise import Model, fields
 from datetime import datetime
-
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 class User(Model):
     id = fields.IntField(pk=True,index=True)
@@ -16,4 +16,6 @@ class Reporter(Model):
     education = fields.CharField(max_length=100, null=False, default = 'Unspecified')
     certificate = fields.CharField(max_length=200, null=False, unique = 'certificate.jpg')
 
-
+user_pydantic = pydantic_model_creator(User, name='User', exclude=('is_verified'))
+user_pydanticIn = pydantic_model_creator(User, name='UserIn',exclude_readonly=True)
+user_pydanticOut = pydantic_model_creator(User, name='UserOut',exclude=('password',))
